ccd32aaf52013275671f6bd26158ed6c2504d720
John Francis Vecina <johnvecina640@gmail.com>
2026-04-12 23:50:02 +0800

    update: enhance project card layout and styling, add source code and live demo buttons

--- utils/Project.js
+++ utils/Project.js
@@ -13,13 +13,20 @@ export class Project {
     }
 
     getProjectCard() {
-        return `<a href="${this.gitUrl}" target="_blank"><img src="${this.imgUrl}" class="project_image"></a>
-							<h2 class="project_head">${this.title}<a href="${this.gitCode}" target="_blank" class="adjust_link"><img src="assets/images/logo/link.png" class="link"></a></h2>
-							<div class="featured_tags btn_js">
-								${this.tags.map((tag, i) => `<a class="tags_btn tags_skill tag project_btn"><img src="${this.tagSvg[i]}" class="skill_logo" id="li_btn"></img>${tag}</a>`).join('')}
+        return `<img src="${this.imgUrl}" class="project_image">
+					<div class="project_contents">
+						<h2 class="project_head">${this.title}</h2>
+						    <div class="featured_tags btn_js">
+							   ${this.tags.map((tag, i) => `<a class="tags_btn tags_skill tag project_btn"><img src="${this.tagSvg[i]}" class="skill_logo" id="li_btn"></img>${tag}</a>`).join('')}
 							</div>
 							<div class="project_divider"></div>
-							<p class="proj_desc">${this.description}</p>`
+								<p class="proj_desc">${this.description}</p>
+							<div class="project_source">
+								<a class="src_button" href="${this.gitUrl}" target="_blank" id="live_btn"><img src="assets/svg/play.svg" alt="" class="src_img" id="play_svg"> Live Demo</a>
+								<a href="${this.gitCode}" class="src_button"><img src="assets/images/logo/github.png" alt="" class="src_img" id="gh_img">Source Code</a>
+							</div>
+					</div>
+				<a href="${this.gitCode}" target="_blank" id="adjust_link"><img src="assets/svg/link.svg" class="link"></a>`
     }
 			
 }
